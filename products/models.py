051bf74be6c74b9c3d9121abe23a298434c0d71f
JosueDLA <josuedlavs@gmail.com>
2020-09-17 22:12:18 -0600

    Created products model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    due_date = models.DateField()
+    stock = models.IntegerField()
+    daily_rate = models.FloatField()
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
