9eb4ea31bfc26c3af925aa5d7b668c28b7287f26
JosueDLA <josuedlavs@gmail.com>
2020-09-18 03:24:00 -0600

    Added products index

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    products = Product.objects.all()
+    return render(request, 'products/index.html', {'products': products})
